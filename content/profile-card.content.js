ed4f433b963fbfeeaa4ca00732713490033fb5e1
armanopbringas <bringas.armandop@gmail.com>
2022-01-20 02:45:28 -0600

    creating card profile component

---
+++ content/profile-card.content.js
@@ -0,0 +1,7 @@
+const profileCardContent = {
+  aboutMyName: "Armando Bringas",
+  aboutRoll: "Frontend developer",
+  aboutMeText: "Hi there, my name is Armando, i´m a frontend developer in training. I achived get my first job learning to code of self-thaught way but right there i realized that i had too much for learn yet. I was a trainee frontend developer at Iceberg9 from september 2020 to december 2020 collaborating as a remote way. Then i was in training in a code bootcam called Bedu, with the objetive to get better my code skills. In my last job i worked for a software consultory applicating what i know and learning too much.",
+}
+
+export default profileCardContent

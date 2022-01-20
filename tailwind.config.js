2eeb3af0bd75600998aa927e0c970459c30ed906
armanopbringas <bringas.armandop@gmail.com>
2022-01-19 23:17:39 -0600

    setting tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mPlusCode: ['M PLUS 1 Code', 'sans-serif'],
+      },
+      colors: {
+        'pink-link': '#ff63c3',
+        'dark-gray': '#202023',
+        'lihgt-gray': '#525252',
+        'light-blue': '#88ccca',
+        'white-text': '#EEEEEE',
+      },
+    },
   },
   plugins: [],
 }

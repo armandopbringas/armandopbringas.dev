18aaae90e0a2d20f9f4b7b0d0e3de943133ba668
armanopbringas <bringas.armandop@gmail.com>
2022-01-22 01:36:47 -0600

    change jobs order

--- content/experience.content.js
+++ content/experience.content.js
@@ -1,17 +1,17 @@
 export const jobExperiences = [
   {
-    jobPosition: "Trainee frontend developer",
-    company: "Icberg 9",
-    timeJob: "sept. 2020 - dic. 2020",
+    jobPosition: "Trainee web developer",
+    company: "Noble",
+    timeJob: "feb. 2021 - dic. 2021",
     jobDescription :`Collaborating as a remote way i developed interfaces with
     flexbox and / or CSS grids to facility the responsive design 
     of websites using Sass and also maintenance of styles and
     content of Shopify projects.`
   },
   {
-    jobPosition: "Trainee web developer",
-    company: "Noble",
-    timeJob: "feb. 2021 - dic. 2021",
+    jobPosition: "Trainee frontend developer",
+    company: "Icberg 9",
+    timeJob: "sept. 2020 - dic. 2020",
     jobDescription :`Collaborating as a remote way i developed interfaces with
     flexbox and / or CSS grids to facility the responsive design 
     of websites using Sass and also maintenance of styles and

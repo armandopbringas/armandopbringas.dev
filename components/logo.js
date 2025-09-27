4b314fb40e4254f865889981b591b082c9f93ff0
Armando Bringas <bringas.armandop@gmail.com>
2025-09-27 12:56:41 -0600

    update nav log

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { Text, useColorModeValue } from '@chakra-ui/react'
-import { DiTerminal } from 'react-icons/di'
+import { MdOutlineQueryStats } from "react-icons/md";
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
@@ -25,7 +25,7 @@ const Logo = () => {
   return (
     <Link href="/" scroll={false}>
       <LogoBox>
-        <DiTerminal size={25} />
+        <MdOutlineQueryStats size={25} />
         <Text
           color={useColorModeValue('gray.800', 'whiteAlpha.900')}
           fontFamily='M PLUS Rounded 1c", sans-serif'
